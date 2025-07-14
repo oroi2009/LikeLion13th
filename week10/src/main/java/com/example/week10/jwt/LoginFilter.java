92903796b8c0441ebf15a2b294c268b321a58f64
oroi2009 <oroi2009@gmail.com>
2025-07-15 02:19:39 +0900

    ✨  feat : DB기반 로그인 검증 로직

--- week10/src/main/java/com/example/week10/jwt/LoginFilter.java
+++ week10/src/main/java/com/example/week10/jwt/LoginFilter.java
@@ -32,11 +32,11 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
-
+        System.out.println("success");
     }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
-
+        System.out.println("fail");
     }
 }

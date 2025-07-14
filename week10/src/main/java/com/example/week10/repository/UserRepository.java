9ed1bc7b8a413f30ea07f61e8377886482d8bc56
oroi2009 <oroi2009@gmail.com>
2025-07-15 00:29:21 +0900

    ♻️  refactor : 불필요 파일 제거

---
+++ week10/src/main/java/com/example/week10/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.week10.repository;
+
+import com.example.week10.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+
+}
